c3cc13703064de3e24cd08b2ee18f5b997c60b0b
Emilio Pomares <epomares@swaap.us>
2023-03-10 10:41:02 +0100

    Added missing boilerplate

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-import FastAPI
+import os
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+import random
 
 app = FastAPI()
 
@@ -13,3 +17,6 @@ async def main():
 @app.get('/authorized')
 async def authorized():
     return {"authorized":True}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
